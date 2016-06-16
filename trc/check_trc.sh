83d0da78c5cc6328942fd4ab34fa58182f37d509
Nikolaos Papanikos <npapanic@gmail.com>
2016-06-16 16:43:17 +0100

    Added missing license notice

--- trc/check_trc.sh
+++ trc/check_trc.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+#  Adyton: A Network Simulator for Opportunistic Networks
+#  Copyright (C) 2015, 2016  Nikolaos Papanikos, Dimitrios-Georgios Akestoridis,
+#  and Evangelos Papapetrou
+#
+#  This file is part of Adyton.
+#
+#  Adyton is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Adyton is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Adyton.  If not, see <http://www.gnu.org/licenses/>.
+#
+#  Written by Nikolaos Papanikos.
+
 RED='\033[0;31m';
 LRED='\033[1;31m';
 GREEN='\033[0;32m';
